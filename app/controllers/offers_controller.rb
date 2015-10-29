5a5e5fdea63e00682a5c0911b21edeabb9967a1f
Oleh Belkot <o.belkot@gmail.com>
2015-10-29 12:19:20 +0200

    Refactoring offers.

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,6 +1,6 @@
 require 'discounts'
 class OffersController < ApplicationController
-  before_action :set_inventory, only: [:constructor, :tester]
+  before_action :set_inventory
 
   def constructor
     @product = Product.new
@@ -11,10 +11,6 @@ class OffersController < ApplicationController
 
     @rule_discount_if_one_by_more = RuleDiscountIfOneByMore.new
     @rule_discount_if_one_by_mores = RuleDiscountIfOneByMore.all
-    respond_to do |format|
-      format.html
-      format.js
-    end
   end
 
   def tester
@@ -26,22 +22,22 @@ class OffersController < ApplicationController
     @rulebyonegetonefree_ids = session[:rulebyonegetonefree_ids]
     @rulediscountifonebymore_ids = session[:rulediscountifonebymore_ids]
 
-    rule_byonegetonefrees = RuleByOneGetOneFree.where id: session[:rulebyonegetonefree_ids]
-    rule_discountifonebymores = RuleDiscountIfOneByMore.where id: session[:rulediscountifonebymore_ids]
-
     rules = []
-    rule_byonegetonefrees.each { |e| rules <<  Rule::ByOneGetOneFree.new(e.product_code) }
-    rule_discountifonebymores.each { |e| rules <<  Rule::DiscountIfOneByeMore.new(product_code: e.product_code, min_count: e.min_count, discount: e.discount) }
-    rules << Rule::Base.new() if rules.empty?
+    RuleByOneGetOneFree.where(id: session[:rulebyonegetonefree_ids]).
+      each { |e| rules << Rule::ByOneGetOneFree.new(e.product_code) }
+
+    RuleDiscountIfOneByMore.where(id: session[:rulediscountifonebymore_ids]).
+      each { |e|
+        rules << Rule::DiscountIfOneByeMore.new(product_code: e.product_code,
+                                                   min_count: e.min_count,
+                                                    discount: e.discount)
+      }
+    rules = Rule::Base.new() if rules.empty?
 
     @co = Checkout.new(*rules)
     scan = session[:scan] || []
     scan.each { |code| @co.scan code }
     @total_price = @co.total
-    respond_to do |format|
-      format.html
-      format.js
-    end
   end
 
   private
