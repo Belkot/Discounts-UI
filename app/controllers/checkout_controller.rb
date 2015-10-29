92743063960ad5bf564de792e076ae529317dfd4
Oleh Belkot <o.belkot@gmail.com>
2015-10-29 11:40:19 +0200

    Refactoring checkout.

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,29 +2,13 @@ require 'discounts'
 class CheckoutController < ApplicationController
 
   def scan
-    Product.all.each do |product|
-      Inventory.add Inventory::Product.new( code:  product.code,
-                                            name:  product.name,
-                                            price: product.price )
-    end
-
     session[:scan] ||= []
     session[:scan] << params[:scan]
 
-    session[:rulebyonegetonefree_ids] ||= []
-    session[:rulediscountifonebymore_ids] ||= []
+    session[:rulebyonegetonefree_ids] = params[:rulebyonegetonefree_ids] || []
+    session[:rulediscountifonebymore_ids] = params[:rulediscountifonebymore_ids] || []
 
-    session[:rulebyonegetonefree_ids] = params[:rulebyonegetonefree_ids]
-    session[:rulediscountifonebymore_ids] = params[:rulediscountifonebymore_ids]
-
-    @rulebyonegetonefree_ids = session[:rulebyonegetonefree_ids]
-    @rulediscountifonebymore_ids = session[:rulediscountifonebymore_ids]
-
-    respond_to do |format|
-      format.html { redirect_to offers_tester_path }
-      format.js { redirect_to offers_tester_path  }
-    end
-
+    redirect_to offers_tester_path
   end
 
   def new
